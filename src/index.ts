ff5bf9c9497aef91e40150a5af0d0d069189f26f
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-11-10 18:58:37 +0100

    add bootstrap fun to src/index

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,17 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }) {
+    try {
+      const knex = strapi.db.connection;
+
+      await knex("components_global_section_rich_texts")
+        .whereRaw(`content IS NULL OR content = '' OR content NOT LIKE '[%'`)
+        .update({ content: "[]" });
+
+      strapi.log.info("✅ Fixed invalid content in section-rich-text");
+    } catch (err) {
+      strapi.log.error("❌ Failed to fix content field:", err);
+    }
+  },
 };
